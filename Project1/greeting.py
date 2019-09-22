64a70cc3d010e95d117e6c516ac471ccbb10a4cb
Raja <khanr@oregonstate.edu>
2019-09-22 13:01:33 -0400

    first project

---
+++ Project1/greeting.py
@@ -0,0 +1,11 @@
+# Author: Raja Khan
+
+# Date: 9/22/2019
+
+# Description: Asks the user for their name and then greets them by name
+
+
+
+your_name = input("Please enter your name: ")
+
+print("Hello " + your_name + "!")
