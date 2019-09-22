0a33d136e11153af2aed4b222ef6f6052ae6e77c
Raja <khanr@oregonstate.edu>
2019-09-22 12:40:36 -0400

    first project

---
+++ Project1/animal.py
@@ -0,0 +1,7 @@
+# Author: Raja Khan
+# Date: 9/22/2019
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
